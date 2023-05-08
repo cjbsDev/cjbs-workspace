4587c48a4120d4588b63d1f97e65175420413c98
masonCJBSFrntDev <musong.choi@cj.net>
2023-05-08 13:57:00 +0900

    test3

--- apps/bsadmin/pages/index.tsx
+++ apps/bsadmin/pages/index.tsx
@@ -1,12 +1,39 @@
+import * as React from 'react';
+import type { ReactElement } from 'react'
+import {Avatar, Typography, Button, Container, Grid, Link}  from '@mui/material';
 import { ContainedButton, OutlinedButton, LinkButton, getCommonLayout } from "ui";
+import type { NextPageWithLayout } from './_app'
 
-export default function Web() {
+function Copyright(props: any) {
   return (
-    <div>
-      <h1>Web</h1>
-      <ContainedButton buttonName='TestName1' />
-      <OutlinedButton buttonName='TestName2' />
-      <LinkButton buttonName='Go to order' pathName='order' />
-    </div>
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+
+      </Link>{' '}
+      {new Date().getFullYear()}
+    </Typography>
   );
 }
+
+
+const Web: NextPageWithLayout = () => {
+  return (
+    <Container component="main" maxWidth="xl" sx={{backgroundColor: '#e8e8e8'}}>
+      <Grid container>
+        <Grid item>
+          <Typography variant="h1">Web</Typography>
+          <ContainedButton buttonName='TestName1' />
+          <OutlinedButton buttonName='TestName2' />
+          <LinkButton buttonName='Go to order' pathName='order' />
+        </Grid>
+      </Grid>
+      <Copyright sx={{ mt: 8, mb: 4 }} />
+    </Container>
+  );
+}
+
+Web.getLayout = getCommonLayout;
+
+export default Web;
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,2 +1,4 @@
 import * as React from "react";
 export * from "./Buttons";
+export * from "./Inputs";
+export * from "./Layouts";
