0a7e3fa61648aa4087581759147641c1d1590802
moonpang <moonpang@cj.net>
2023-05-22 14:46:33 +0900

    커밋!!

---
+++ apps/meta/src/component/organisms/clinical/dashboard/clinicalDashboard.tsx
@@ -0,0 +1,77 @@
+import { Box, Grid, styled } from '@mui/material';
+import React from 'react';
+import DashboardItem from '../../../molecules/dashboard/dashboardItem';
+import Person from '../../../../../public/img/dashboard/person.png';
+import Disease from '../../../../../public/img/dashboard/disease.png';
+import Check from '../../../../../public/img/dashboard/check.png';
+import Sylinder from '../../../../../public/img/dashboard/sylinder.png';
+
+const ClinicalDashboard = () => {
+  return (
+    <GridContainer container>
+      <FlexCenterGrid xs={4} item></FlexCenterGrid>
+      <FlexCenterGrid xs={8} item>
+        <ItemsBox>
+          <RightBorderBox sx={{}}>
+            <DashboardItem
+              imgPath={Disease}
+              name={'총 질환 수'}
+              text={'1,605'}
+            />
+          </RightBorderBox>
+          <RightBorderBox>
+            <DashboardItem
+              imgPath={Sylinder}
+              name={'총 샘플 수'}
+              text={'1,605'}
+            />
+          </RightBorderBox>
+          <RightBorderBox>
+            <DashboardItem
+              imgPath={Person}
+              name={'총 대상자 수'}
+              text={'1,605'}
+            />
+          </RightBorderBox>
+          <RightBorderBox sx={{ borderRight: 'none' }}>
+            <DashboardItem
+              imgPath={Check}
+              name={'총 업데이트 일자'}
+              text={'1,605'}
+            />
+          </RightBorderBox>
+        </ItemsBox>
+      </FlexCenterGrid>
+    </GridContainer>
+  );
+};
+
+const GridContainer = styled(Grid)`
+  box-sizing: border-box;
+  height: 222px;
+  background: #f8f9fa;
+  border: 1px solid #e2e8f0;
+  border-radius: 2px;
+`;
+
+const ItemsBox = styled(Grid)`
+  display: flex;
+  justify-content: space-evenly;
+  width: 764px;
+  height: 118px;
+`;
+
+const FlexCenterGrid = styled(Grid)`
+  display: flex;
+  align-items: center;
+`;
+
+export default ClinicalDashboard;
+
+const RightBorderBox = styled(Box)`
+  border-right: 1px solid #ced4da;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  width: 100%;
+`;
