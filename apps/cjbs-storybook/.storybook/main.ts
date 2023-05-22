8cae1ee8a0e58105b29fb7d54d0b43e5affed895
masonCJBSFrntDev <musong.choi@cj.net>
2023-05-22 16:25:04 +0900

    - Storybook setting....

--- apps/cjbs-storybook/.storybook/main.ts
+++ apps/cjbs-storybook/.storybook/main.ts
@@ -1,14 +1,13 @@
-import type { StorybookConfig } from "@storybook/nextjs";
+import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
-  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    '@storybook/preset-create-react-app',
   ],
   framework: {
-    name: "@storybook/nextjs",
+    name: "@storybook/react-vite",
     options: {},
   },
   docs: {
--- apps/cjbs-storybook/src/stories/Button.tsx
+++ apps/cjbs-storybook/src/stories/Button.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './button.css';
-import { useRouter } from 'next/navigation';
 
 interface ButtonProps {
   /**
@@ -36,20 +35,14 @@ export const Button = ({
   ...props
 }: ButtonProps) => {
   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
-  // const router = useRouter();
-
   return (
     <button
       type="button"
       className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      style={{ backgroundColor }}
       {...props}
     >
       {label}
-      <style jsx>{`
-        button {
-          background-color: ${backgroundColor};
-        }
-      `}</style>
     </button>
   );
 };
