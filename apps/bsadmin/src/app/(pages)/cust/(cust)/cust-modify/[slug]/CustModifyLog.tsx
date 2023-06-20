67234d69ea50a84d122794060f4bf549eeb3a1c9
icranCJ <hyungseok.seo@cj.net>
2023-06-20 16:40:38 +0900

    고객 정보 수정 히스토리 기능 개발

--- apps/bsadmin/src/app/(pages)/cust/(cust)/cust-modify/[slug]/CustModifyLog.tsx
+++ apps/bsadmin/src/app/(pages)/cust/(cust)/cust-modify/[slug]/CustModifyLog.tsx
@@ -3,50 +3,80 @@
 import React from "react";
 import { DataTableBase } from "cjbsDSTM";
 import { TableColumn } from "react-data-table-component";
+import { Box, Stack } from "@mui/material";
+
 import useSWR from "swr";
 import axios from "axios";
 import { dataTableCustomStyles2 } from "cjbsDSTM/organisms/DataTable/style/dataTableCustomStyle";
 
 const fetcher = (url: string) => axios.get(url).then((res) => res.data);
-const CustModifyLog = () => {
-  const { data } = useSWR(`https://dummyjson.com/products`, fetcher, {
-    suspense: true,
-  });
+
+interface CustModifyLogProps {
+  slug: string;
+  ebcShow: boolean;
+}
+
+const CustModifyLog: React.FC<CustModifyLogProps> = ({ slug }) => {
+  const { data: custModifyLogTemp, error: custModifyLogError } = useSWR(
+    `http://cjbs-it-alb-980593920.ap-northeast-2.elb.amazonaws.com:9000/cust/list/detail/${slug}/hstr?page=0&size=50`,
+    fetcher
+  );
+
+  if (!custModifyLogTemp) {
+    return <div>Cust Modify log Loading...</div>;
+  }
+
+  const custModifyLogList = custModifyLogTemp.data.hstrList;
 
   const columns = [
     {
       name: "변경일",
-      selector: (row: { id: number }) => row.id,
+      selector: (row: { modifiedAt: any }) => row.modifiedAt,
       width: "20%",
     },
     {
-      name: "관리자ID",
-      selector: (row: { stock: any }) => row.stock,
+      name: "변경자",
+      cell: (row: { updatedByNm: any; updatedByEmail: any }) => (
+        <>
+          <Stack
+            direction="row"
+            spacing={0.4}
+            alignItems="center"
+            useFlexGap
+            flexWrap="wrap"
+          >
+            <Box>{row.updatedByNm}</Box>
+            <Box>( {row.updatedByEmail} )</Box>
+          </Stack>
+        </>
+      ),
+      minWidth: "150px",
       width: "20%",
     },
+
     {
-      name: "이름",
-      selector: (row: { brand: any }) => row.brand,
+      name: "컬럼",
+      selector: (row: { targetColNm: any }) => row.targetColNm,
       width: "20%",
     },
     {
       name: "변경 전",
-      selector: (row: { rating: any }) => row.rating,
+      selector: (row: { preUpdateValue: any }) => row.preUpdateValue,
       width: "20%",
     },
     {
       name: "변경 후",
-      selector: (row: { rating: any }) => row.rating,
+      selector: (row: { postUpdateValue: any }) => row.postUpdateValue,
       width: "20%",
     },
   ];
   return (
     <DataTableBase
-      data={data.products}
+      data={custModifyLogList}
       columns={columns}
       selectableRows={false}
-      paginationPerPage={5}
-      paginationRowsPerPageOptions={[5, 10, 15]}
+      paginationPerPage={50}
+      paginationRowsPerPageOptions={[20, 50, 100]}
       customStyles={dataTableCustomStyles2}
     />
   );
