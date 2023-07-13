f91a8b82e776c5eeaa52110d4341ff13d2e30c19
yangkyu.choi <yangkyu.choi@cj.net>
2023-07-13 11:18:37 +0900

    project-list/page.tsx - bulid 이슈로 수정

---
+++ apps/bsadmin/src/app/(pages)/set/project-list/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import * as React from "react";
+import { ErrorContainer, Fallback } from "cjbsDSTM";
+
+// bulid 이슈로 임의작성 삭제해주세요
+export default function ManagementPage() {
+    return (
+        <ErrorContainer FallbackComponent={Fallback}>
+        </ErrorContainer>
+    );
+}
