1c4085f922d971d1c1bb14af813a790856e50446
icranCJ <hyungseok.seo@cj.net>
2023-08-01 13:48:07 +0900

    개발 진행중

--- apps/bsadmin/src/app/(pages)/set/contact-list/page.tsx
+++ apps/bsadmin/src/app/(pages)/set/contact-list/page.tsx
@@ -1,14 +1,16 @@
-"use client";
-import * as React from "react";
 import dynamic from "next/dynamic";
 import SkeletonLoading from "../../../components/SkeletonLoading";
 import { ErrorContainer, Fallback } from "cjbsDSTM";
-import { Chip, Stack, Grid, Box, Typography } from "@mui/material";
+
+const LazyListCust = dynamic(() => import("./ListCust"), {
+  ssr: false,
+  loading: () => <SkeletonLoading height={800} />,
+});
 
 export default function ManagementPage() {
   return (
-    <Grid>
-      <Stack>8월 30일 오픈 예정</Stack>
-    </Grid>
+    <ErrorContainer FallbackComponent={Fallback}>
+      <LazyListCust />
+    </ErrorContainer>
   );
 }
