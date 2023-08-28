3f4d7e1fb4c205a844a60caa9a66036eb87a4a0d
masonCJBSFrntDev <musong.choi@cj.net>
2023-08-28 10:08:30 +0900

    오더 정보[샘플] -샘플정보에 변경로그리스트 추가

--- apps/bsadmin/src/app/(pages)/(order)/order-list/[slug]/(SampleTab)/(SampleInfoModal)/SampleInfoModal.tsx
+++ apps/bsadmin/src/app/(pages)/(order)/order-list/[slug]/(SampleTab)/(SampleInfoModal)/SampleInfoModal.tsx
@@ -1,15 +1,24 @@
 import React from "react";
-import { ModalContainer, ModalTitle } from "cjbsDSTM";
+import { ErrorContainer, Fallback, ModalContainer, ModalTitle } from "cjbsDSTM";
 import { DialogContent } from "@mui/material";
 import { ModalContainerProps } from "../../../../../../types/ModalContainerProps";
 import useSWR from "swr";
 import fetcher from "../../../../../../func/fetcher";
 import SampleInfoTable1 from "./SampleInfoTable1";
 import SampleInfoTable2 from "./SampleInfoTable2";
+import dynamic from "next/dynamic";
+import SkeletonLoading from "../../../../../../components/SkeletonLoading";
 
 interface SampleInfoModalProps extends ModalContainerProps {
   sampleUkey: string;
 }
+const LazyAgncModifyLog = dynamic(
+  () => import("../../../../../../components/LogTable"),
+  {
+    ssr: false,
+    loading: () => <SkeletonLoading height={142} />,
+  }
+);
 
 const SampleInfoModal = (props: SampleInfoModalProps) => {
   const { onClose, open, modalWidth, sampleUkey } = props;
@@ -29,6 +38,9 @@ const SampleInfoModal = (props: SampleInfoModalProps) => {
       <DialogContent>
         <SampleInfoTable1 sampleInfoData={sampleInfoData} />
         <SampleInfoTable2 sampleStatusRes={sampleStatusRes} />
+        <ErrorContainer FallbackComponent={Fallback}>
+          <LazyAgncModifyLog apiName="sample/status" uKey={sampleUkey} />
+        </ErrorContainer>
       </DialogContent>
     </ModalContainer>
   );
