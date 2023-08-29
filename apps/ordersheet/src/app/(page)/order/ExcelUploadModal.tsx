e16e0e67fb21885f7d6bfad3ac68b5c0e3ce8452
masonCJBSFrntDev <musong.choi@cj.net>
2023-08-29 16:51:15 +0900

    외부주문서 -샘플 리스트 이슈 수정.

--- apps/ordersheet/src/app/(page)/order/ExcelUploadModal.tsx
+++ apps/ordersheet/src/app/(page)/order/ExcelUploadModal.tsx
@@ -1,150 +1,129 @@
 import React, { useState, useMemo } from "react";
 import {
-    ModalContainer,
-    ModalTitle,
-    ModalAction,
-    Form,
-    InputValidation,
-    ContainedButton, cjbsTheme,
+  ModalContainer,
+  ModalTitle,
+  ModalAction,
+  Form,
+  InputValidation,
+  ContainedButton,
+  cjbsTheme,
 } from "cjbsDSTM";
-import {
-    DialogContent,
-    Stack,
-    styled,
-    Typography,
-} from "@mui/material";
+import { DialogContent, Stack, styled, Typography } from "@mui/material";
 import { LoadingButton } from "@mui/lab";
 import axios from "axios";
 import MyIcon from "icon/myIcon";
 import Link from "next/link";
 
-
 interface ModalContainerProps {
-    onClose: () => void;
-    open: boolean;
-    modalWidth: number;
-    addExcelDataTableRows:object;
+  onClose: () => void;
+  open: boolean;
+  modalWidth: number;
+  append: any;
 }
 
-const ExcelUploadModal = ({ onClose,
-                            open,
-                            modalWidth,
-                            addExcelDataTableRows,
-}: // data,
-    ModalContainerProps) => {
-    const [isLoading, setIsLoading] = useState<boolean>(false);
+const ExcelUploadModal = ({ onClose, open, modalWidth, append }) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-    const handleClose = () => {
-        onClose();
-        setIsLoading(false);
-    };
+  const handleClose = () => {
+    onClose();
+    setIsLoading(false);
+  };
 
-    const defaultValues = {};
+  const handleFileChange = async (event) => {
+    const file = event.target.files[0]; // 선택된 파일 객체
+    console.log("File Input Data ==>>", file);
 
-    const onSubmit = async (data: any, e: any) => {
-        e.preventDefault();
-        setIsLoading(true);
-        console.log("엑셀 업로드 데이터 Body Data ==>>", data);
+    if (file) {
+      const formData = new FormData();
+      formData.append("file", file as File);
 
-        if(data.fileInput[0]){
-            const formData = new FormData();
-            formData.append("file", data.fileInput[0] as File);
+      await axios
+        .post(
+          `${process.env.NEXT_PUBLIC_API_URL_ORSH}/sample/excel/mtp/fs`,
+          formData,
+          {
+            withCredentials: false,
+            headers: {
+              "Access-Control-Allow-Origin": "*",
+            },
+          }
+        )
+        .then((res) => {
+          if (res.data.success) {
+            console.log("RES VALUE ==>>", res.data.data);
+            console.log("RES VALUE Length ==>>", res.data.data.length);
 
-            const res = await axios.post<{ url: string }>(
-                `${process.env.NEXT_PUBLIC_API_URL_ORSH}/sample/excel/mtp/fs`,
-                formData,
-                {
-                    withCredentials: false,
-                    headers: {
-                        "Access-Control-Allow-Origin": "*",
-                    },
-                },
-            );
-            console.log("res", res);
-            addExcelDataTableRows(res.data.data);
+            for (let i = 0; i < res.data.data.length; i++) {
+              append({
+                sampleNm: res.data.data[i].sampleNm,
+                source: res.data.data[i].source,
+                sampleCategoryCc: res.data.data[i].sampleCategoryCc,
+                anlsTargetGene: res.data.data[i].anlsTargetGene,
+                qc: res.data.data[i].qc,
+                memo: res.data.data[i].memo,
+              });
+            }
+
             handleClose();
-        } else {
+          } else {
+            console.log("EERROORRSS!!");
+          }
+        });
+    }
+  };
 
-        }
-        setIsLoading(false);
-    };
-
-    return (
-        <ModalContainer onClose={handleClose} open={open} modalWidth={modalWidth}>
-            <ModalTitle onClose={handleClose}>엑셀 등록</ModalTitle>
-            <DialogContent>
-                <Form
-                    onSubmit={onSubmit}
-                    defaultValues={defaultValues}
-                    id="excelUploadForm"
-                >
-                    <Stack
-                        alignItems="start"
-                        spacing={2}
-                        sx={{
-                            p: "20px",
-                            backgroundColor: cjbsTheme.palette.grey[50],
-                        }}
-                    >
-                        <Typography variant="body2">• 엑셀 양식을 다운로드한 후 데이터를 입력한 파일을 업로드해주세요.</Typography>
-                        <Link href="https://bsa-public-resource.s3.ap-northeast-2.amazonaws.com/MTP_Full_service_template.xlsx" target="_blank">
-                            <ContainedButton
-                                buttonName="엑셀 양식 다운로드"
-                                startIcon={<MyIcon icon="download" size={16}/>}
-                                color={"secondary"}
-                                size="small"
-                                sx={{marginLeft: '20spx !important'}}
-                            />
-                        </Link>
-                        {/*https://bsa-public-resource.s3.ap-northeast-2.amazonaws.com/MTP_Analysis_only_template.xlsx*/}
-                        {/*https://bsa-public-resource.s3.ap-northeast-2.amazonaws.com/MTP_Sequencing_only_template.xlsx*/}
-                        <Typography variant="body2">• 엑셀 등록 시 기존에 입력한 정보는 초기화됩니다.</Typography>
-                    </Stack>
-                    <Stack
-                        direction="row"
-                        alignItems="center"
-                        justifyContent="center"
-                        spacing={1}
-                        sx={{
-                            mt: "20px",
-                        }}
-                    >
-                        {/*<input id="fileInput" type="file" style={{width:"600px"}}/>*/}
-                        <InputValidation
-                            inputName="fileInput"
-                            required={false}
-                            type="file"
-                            sx={{ width: 600 }}
-                        />
-                        <LoadingButton
-                            loading={isLoading}
-                            variant="contained"
-                            type="submit"
-                            form="excelUploadForm"
-                        >
-                            파일 업로드
-                        </LoadingButton>
-                        {/*<OutlinedButton*/}
-                        {/*    buttonName="식제"*/}
-                        {/*    color={"error"}*/}
-                        {/*/>*/}
-                    </Stack>
-                </Form>
-            </DialogContent>
-            {/*<ModalAction>*/}
-            {/*    <OutlinedButton*/}
-            {/*        buttonName="취소"*/}
-            {/*        onClick={handleClose}*/}
-            {/*        color="secondary"*/}
-            {/*    />*/}
-            {/*    <LoadingButton*/}
-            {/*        variant="contained"*/}
-            {/*    >*/}
-            {/*        확인*/}
-            {/*    </LoadingButton>*/}
-            {/*</ModalAction>*/}
-        </ModalContainer>
-    );
+  return (
+    <ModalContainer onClose={handleClose} open={open} modalWidth={modalWidth}>
+      <ModalTitle onClose={handleClose}>엑셀 등록</ModalTitle>
+      <DialogContent>
+        <Stack
+          alignItems="start"
+          spacing={2}
+          sx={{
+            p: "20px",
+            backgroundColor: cjbsTheme.palette.grey[50],
+          }}
+        >
+          <Typography variant="body2">
+            • 엑셀 양식을 다운로드한 후 데이터를 입력한 파일을 업로드해주세요.
+          </Typography>
+          <Link
+            href="https://bsa-public-resource.s3.ap-northeast-2.amazonaws.com/MTP_Full_service_template.xlsx"
+            target="_blank"
+          >
+            <ContainedButton
+              buttonName="엑셀 양식 다운로드"
+              startIcon={<MyIcon icon="download" size={16} />}
+              color={"secondary"}
+              size="small"
+              sx={{ marginLeft: "20spx !important" }}
+            />
+          </Link>
+          {/*https://bsa-public-resource.s3.ap-northeast-2.amazonaws.com/MTP_Analysis_only_template.xlsx*/}
+          {/*https://bsa-public-resource.s3.ap-northeast-2.amazonaws.com/MTP_Sequencing_only_template.xlsx*/}
+          <Typography variant="body2">
+            • 엑셀 등록 시 기존에 입력한 정보는 초기화됩니다.
+          </Typography>
+        </Stack>
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="center"
+          spacing={1}
+          sx={{
+            mt: "20px",
+          }}
+        >
+          <input
+            id="fileInput"
+            type="file"
+            onChange={handleFileChange}
+            style={{ width: "600px" }}
+          />
+        </Stack>
+      </DialogContent>
+    </ModalContainer>
+  );
 };
 
 export default ExcelUploadModal;
---
+++ apps/ordersheet/src/app/(page)/order/OrderMTPSampleDynamicTable.tsx
@@ -0,0 +1,127 @@
+import React, { useState } from "react";
+import {
+  Box,
+  IconButton,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import {
+  cjbsTheme,
+  ContainedButton,
+  InputValidation,
+  SelectBox,
+  UnStyledButton,
+} from "cjbsDSTM";
+import { useFieldArray, useFormContext } from "react-hook-form";
+import MyIcon from "icon/myIcon";
+import axios from "axios";
+import ExRow from "@app/(page)/order/ExRow";
+import TableHeader from "@app/(page)/order/TableHeader";
+import TableNewRows from "@app/(page)/order/TableNewRows";
+import ExcelUploadModal from "@app/(page)/order/ExcelUploadModal";
+
+function getUserAccount() {
+  return axios.get(
+    `${process.env.NEXT_PUBLIC_API_URL}/code/list/shortly/value?topValue=sample&midValue=category`
+  );
+}
+
+function getUserPermissions() {
+  return axios.get(
+    `${process.env.NEXT_PUBLIC_API_URL}/code/list/shortly/value?topValue=sample&midValue=genome`
+  );
+}
+
+let acct: any;
+let perm: any;
+Promise.all([getUserAccount(), getUserPermissions()]) // Promise, then 사용
+  .then(function (results) {
+    // 응답 결과를 results 배열로 받아서
+    acct = results[0].data.data; // 각각의 결과를 acct와 perm에 저장
+    perm = results[1].data.data;
+  });
+
+const OrderMTPSampleDynamicTable = () => {
+  const { watch, control, getValues, formState } = useFormContext();
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "sample", // 이름은 폼 데이터에 저장될 필드 이름입니다.
+  });
+  const { errors } = formState;
+  const [showOrderInfoModifyModal, setShowOrderInfoModifyModal] =
+    useState<boolean>(false);
+  const orderInfoModifyModalClose = () => {
+    setShowOrderInfoModifyModal(false);
+  };
+
+  const handleAddFields = (count) => {
+    console.log("Count~!~!", count);
+    for (let i = 0; i < count; i++) {
+      append({
+        sampleNm: "",
+        source: "",
+        sampleCategoryCc: "",
+        anlsTargetGene: "",
+        qc: "",
+        memo: "",
+      }); // 입력된 수만큼 항목을 추가합니다.
+    }
+  };
+  return (
+    <>
+      <Stack direction="row">
+        <UnStyledButton
+          sx={{}}
+          buttonName="엑셀 등록"
+          startIcon={<MyIcon icon="xls3" size={18} />}
+          size="small"
+          onClick={() => setShowOrderInfoModifyModal(true)}
+        />
+        <ExcelUploadModal
+          onClose={orderInfoModifyModalClose}
+          open={showOrderInfoModifyModal}
+          modalWidth={800}
+          append={append}
+          handleAddFields={handleAddFields}
+          // addExcelDataTableRows={addExcelDataTableRows}
+        />
+        <InputValidation inputName="count" type="number" />
+        <ContainedButton
+          buttonName="행 추가"
+          size="small"
+          color={"secondary"}
+          onClick={() => handleAddFields(getValues("count"))}
+        />
+      </Stack>
+      <TableContainer sx={{ mb: 5, mt: 1, borderTop: "1px solid #000" }}>
+        <Table>
+          <TableHeader />
+          <TableBody>
+            <ExRow />
+            {fields.map((field, index) => {
+              return (
+                <TableNewRows
+                  key={field.id}
+                  field={field}
+                  remove={remove}
+                  index={index}
+                  acct={acct}
+                  perm={perm}
+                  errors={errors}
+                />
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+};
+
+export default OrderMTPSampleDynamicTable;
