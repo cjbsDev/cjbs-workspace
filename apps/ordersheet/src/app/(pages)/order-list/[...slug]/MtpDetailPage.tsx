360e7955d4979dbf47548e4b82b2f904524bb21f
yangkyu.choi <yangkyu.choi@cj.net>
2023-10-16 15:11:40 +0900

    외부 주문서 - RS 조회, 수정 페이지 추가

--- apps/ordersheet/src/app/(pages)/order-list/[...slug]/MtpDetailPage.tsx
+++ apps/ordersheet/src/app/(pages)/order-list/[...slug]/MtpDetailPage.tsx
@@ -4,24 +4,20 @@ import {cjbsTheme, ConfirmModal, ErrorContainer, Fallback, Title1} from "cjbsDST
 import { useRouter } from "next-nprogress-bar";
 import {Box, Container, Stack, styled, Typography} from "@mui/material";
 import * as React from "react";
-import MtpFullService from "@app/(pages)/order-list/[...slug]/(mtp)/(service)/MtpFullService";
-import MtpAnalysis from "@app/(pages)/order/mtp/(service)/MtpAnalysis";
-import MtpSequencing from "@app/(pages)/order/mtp/(service)/MtpSequencing";
-import {useState} from "react";
 import dynamic from "next/dynamic";
 import SkeletonLoading from "@components/SkeletonLoading";
 import {useParams} from "next/navigation";
 
 
-const LazyMtpFullService = dynamic(() => import("@app/(pages)/order-list/[...slug]/(mtp)/(service)/MtpFullService"), {
+const LazyMtpFullService = dynamic(() => import("./(mtp)/(service)/MtpFullService"), {
   ssr: false,
   loading: () => <SkeletonLoading height={800} />,
 });
-const LazyMtpAnalysis = dynamic(() => import("@app/(pages)/order-list/[...slug]/(mtp)/(service)/MtpAnalysis"), {
+const LazyMtpAnalysis = dynamic(() => import("./(mtp)/(service)/MtpAnalysis"), {
   ssr: false,
   loading: () => <SkeletonLoading height={800} />,
 });
-const LazyMtpSequencing = dynamic(() => import("@app/(pages)/order-list/[...slug]/(mtp)/(service)/MtpSequencing"), {
+const LazyMtpSequencing = dynamic(() => import("./(mtp)/(service)/MtpSequencing"), {
   ssr: false,
   loading: () => <SkeletonLoading height={800} />,
 });
@@ -38,8 +34,6 @@ export default function OrshMtpDetailPage() {
     <Container disableGutters={true}>
       <Box
         sx={{
-          // mb: 20,
-          // mt: 11,
           display: 'flex',
           justifyContent: 'space-between',
           alignItems: 'space-between',
