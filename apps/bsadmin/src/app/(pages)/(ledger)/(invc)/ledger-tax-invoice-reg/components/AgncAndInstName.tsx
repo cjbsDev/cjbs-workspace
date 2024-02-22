941b08cc80607043c3598d14c38559a2f66047e7
mason <choimusong0529@gmail.com>
2024-02-22 16:07:08 +0900

    BSAdmin 세금계산서 등록 -거래처 모달 -> 팝업으로

--- apps/bsadmin/src/app/(pages)/(ledger)/(invc)/ledger-tax-invoice-reg/components/AgncAndInstName.tsx
+++ apps/bsadmin/src/app/(pages)/(ledger)/(invc)/ledger-tax-invoice-reg/components/AgncAndInstName.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useFormContext } from "react-hook-form";
 import { Typography } from "@mui/material";
 
-const AgncAndInstName = () => {
+const AgncAndInstName = ({}) => {
   const { getValues } = useFormContext();
   const getAgncNm = getValues("agncNm");
   const getAgncInstNm = getValues("agncInstNm");
@@ -14,11 +14,7 @@ const AgncAndInstName = () => {
   const displayText = isEmpty ? "거래처 검색을 해주세요." : `${getAgncInstNm}`;
 
   return (
-    <Typography
-      variant="body2"
-      color="secondary"
-      // sx={{ color: isEmpty && "red" }}
-    >
+    <Typography variant="body2" color="secondary">
       {displayText}
     </Typography>
   );
