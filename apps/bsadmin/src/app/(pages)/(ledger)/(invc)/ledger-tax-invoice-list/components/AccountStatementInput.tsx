323e791b7c2b9da42174c88989f095402076487f
mason <choimusong0529@gmail.com>
2024-03-26 11:08:12 +0900

    세금계산서(발행) -발행 정보 변경시 모달에 기존 정보가 조회되도록 수정

--- apps/bsadmin/src/app/(pages)/(ledger)/(invc)/ledger-tax-invoice-list/components/AccountStatementInput.tsx
+++ apps/bsadmin/src/app/(pages)/(ledger)/(invc)/ledger-tax-invoice-list/components/AccountStatementInput.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { useFormContext, Controller } from "react-hook-form";
-import { NumericFormat, PatternFormat } from "react-number-format";
-import { cjbsTheme, InputEAType, InputNumberType } from "cjbsDSTM";
+import { PatternFormat } from "react-number-format";
+import { cjbsTheme, InputNumberType } from "cjbsDSTM";
 import { Stack, Typography } from "@mui/material";
 
 const AccountStatementInput = () => {
   const {
     control,
-    watch,
     formState: { errors },
   } = useFormContext();
   return (
@@ -17,17 +16,6 @@ const AccountStatementInput = () => {
         control={control}
         rules={{ required: "세금계산서 번호를 다시 한번 확인해 주세요." }}
         render={({ field: { onChange, value }, fieldState: { error } }) => (
-          // <NumericFormat
-          //   defaultValue={0}
-          //   value={value}
-          //   thousandSeparator={true}
-          //   allowNegative={false}
-          //   onValueChange={(values) => {
-          //     onChange(values.floatValue); // 또는 `values.value`를 사용하여 문자열로 처리
-          //   }}
-          //   customInput={InputEAType}
-          // />
-
           <PatternFormat
             format="########-########-########"
             value={value}
