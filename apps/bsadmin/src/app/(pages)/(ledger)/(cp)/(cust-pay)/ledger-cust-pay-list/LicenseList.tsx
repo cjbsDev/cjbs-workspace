db744d7760ed85ffe6759731aefa8404832808a6
Mason <choimusong0529@gmail.com>
2024-05-08 10:46:18 +0900

    고객 별 결제 현황 라이선스 탭 -> 년/월 최신화 이슈 수정

--- apps/bsadmin/src/app/(pages)/(ledger)/(cp)/(cust-pay)/ledger-cust-pay-list/LicenseList.tsx
+++ apps/bsadmin/src/app/(pages)/(ledger)/(cp)/(cust-pay)/ledger-cust-pay-list/LicenseList.tsx
@@ -5,17 +5,19 @@ import {
   DataCountResultInfo,
   DataTableBase,
   cjbsTheme,
-  FileDownloadBtn, green, red, grey, TH, TD,
+  FileDownloadBtn,
 } from "cjbsDSTM";
-import {
-  blue,
-} from "cjbsDSTM/themes/color";
+import { blue } from "cjbsDSTM/themes/color";
 import {
   Box,
   Stack,
   Grid,
   Typography,
-  TableContainer, Table, TableBody, TableCell, TableRow, Chip, FormControlLabel, FormControl, Select, MenuItem,
+  TableContainer,
+  Table,
+  TableBody,
+  TableCell,
+  TableRow,
 } from "@mui/material";
 import { useRouter } from "next-nprogress-bar";
 import { useState } from "react";
@@ -27,17 +29,19 @@ import { useSearchParams } from "next/navigation";
 import KeywordSearch from "../../../../../components/KeywordSearch";
 import NoDataView from "../../../../../components/NoDataView";
 import { ExpanderComponentProps } from "react-data-table-component";
-import ResultInSearch from "./ResultInSearch";
 import YearMonthSelectBox from "./components/YearMonthSelectBox";
-import {useFormContext} from "react-hook-form";
 import TotalPriceBox from "./components/TotalPriceBox";
+import dayjs from "dayjs";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { currentMonthAtom, currentYearAtom } from "./atom";
+// const currentYear = dayjs().year();
+// const currentMonth = dayjs().get("month");
 
 const LicenseList = () => {
   const [page, setPage] = useState<number>(1);
   const [size, setSize] = useState<number>(20);
-  const [year, setYear] = useState<string>("2024");
-  const [month, setMonth] = useState<string>("03");
-
+  const year = useRecoilValue(currentYearAtom);
+  const month = useRecoilValue(currentMonthAtom);
   const searchParams = useSearchParams();
   const resultObject: any = {};
 
@@ -56,13 +60,11 @@ const LicenseList = () => {
     fetcher,
     {
       suspense: true,
-    }
+    },
   );
   console.log("RUN LIST DATA", data);
-  const licenseStatusList = data.licenseStatusList;
-  const totalElements = data.pageInfo.totalElements;
-  const yearTotalPrice = data.yearTotalPrice;
-  const monthTotalPrice = data.monthTotalPrice;
+  const { licenseStatusList, yearTotalPrice, monthTotalPrice, pageInfo } = data;
+  const { totalElements } = pageInfo;
 
   const [filterText, setFilterText] = useState("");
   const router = useRouter();
@@ -71,14 +73,30 @@ const LicenseList = () => {
   const columns = useMemo(
     () => [
       {
-        name: <Stack justifyContent="center" alignItems="center" sx={{width:'100%'}}><Typography variant="body2">분석내역서</Typography></Stack>,
+        name: (
+          <Stack
+            justifyContent="center"
+            alignItems="center"
+            sx={{ width: "100%" }}
+          >
+            <Typography variant="body2">분석내역서</Typography>
+          </Stack>
+        ),
         width: "110px",
         center: true,
         // sortable: true,
         selector: (row: any) => row.anlsItstId,
       },
       {
-        name: <Stack justifyContent="center" alignItems="center" sx={{width:'100%'}}><Typography variant="body2">거래처(PI)</Typography></Stack>,
+        name: (
+          <Stack
+            justifyContent="center"
+            alignItems="center"
+            sx={{ width: "100%" }}
+          >
+            <Typography variant="body2">거래처(PI)</Typography>
+          </Stack>
+        ),
         width: "300px",
         // sortable: true,
         // selector: (row : {agncNm: string; instNm: string}) => row.agncNm,
@@ -101,71 +119,120 @@ const LicenseList = () => {
         },
       },
       {
-        name: <Stack justifyContent="center" alignItems="center" sx={{width:'100%'}}><Typography variant="body2">연구 책임자</Typography></Stack>,
+        name: (
+          <Stack
+            justifyContent="center"
+            alignItems="center"
+            sx={{ width: "100%" }}
+          >
+            <Typography variant="body2">연구 책임자</Typography>
+          </Stack>
+        ),
         width: "130px",
         center: true,
         // sortable: true,
         selector: (row: any) => row.rhpiNm,
       },
       {
-        name: <Stack justifyContent="center" alignItems="center" sx={{width:'100%'}}><Typography variant="body2">영업 담당자</Typography></Stack>,
+        name: (
+          <Stack
+            justifyContent="center"
+            alignItems="center"
+            sx={{ width: "100%" }}
+          >
+            <Typography variant="body2">영업 담당자</Typography>
+          </Stack>
+        ),
         width: "130px",
         center: true,
         // sortable: true,
         selector: (row: any) => row.bsnsMngrNm,
       },
       {
-        name: <Stack justifyContent="center" alignItems="center" sx={{width:'100%'}}><Typography variant="body2">공급가액</Typography></Stack>,
+        name: (
+          <Stack
+            justifyContent="center"
+            alignItems="center"
+            sx={{ width: "100%" }}
+          >
+            <Typography variant="body2">공급가액</Typography>
+          </Stack>
+        ),
         width: "150px",
         right: true,
         // sortable: true,
-        selector: (row: any) => row.totalSupplyPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+        selector: (row: any) =>
+          row.totalSupplyPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","),
       },
       {
-        name: <Stack justifyContent="center" alignItems="center" sx={{width:'100%'}}><Typography variant="body2">사용 기간</Typography></Stack>,
+        name: (
+          <Stack
+            justifyContent="center"
+            alignItems="center"
+            sx={{ width: "100%" }}
+          >
+            <Typography variant="body2">사용 기간</Typography>
+          </Stack>
+        ),
         width: "200px",
         center: true,
         // sortable: true,
-        selector: (row: any) => row.startDate+" ~ "+row.endDate,
+        selector: (row: any) => row.startDate + " ~ " + row.endDate,
       },
       {
-        name: <Stack justifyContent="center" alignItems="center" sx={{width:'100%'}}><Typography variant="body2">회차</Typography></Stack>,
+        name: (
+          <Stack
+            justifyContent="center"
+            alignItems="center"
+            sx={{ width: "100%" }}
+          >
+            <Typography variant="body2">회차</Typography>
+          </Stack>
+        ),
         width: "100px",
         center: true,
         // sortable: true,
-        selector: (row: any) => row.currentTurn+" / "+row.totalTurn,
+        selector: (row: any) => row.currentTurn + " / " + row.totalTurn,
       },
       {
-        name: <Stack justifyContent="center" alignItems="center" sx={{width:'100%'}}><Typography variant="body2">사용금액</Typography></Stack>,
+        name: (
+          <Stack
+            justifyContent="center"
+            alignItems="center"
+            sx={{ width: "100%" }}
+          >
+            <Typography variant="body2">사용금액</Typography>
+          </Stack>
+        ),
         width: "110px",
         right: true,
         // sortable: true,
-        selector: (row: any) => row.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+        selector: (row: any) =>
+          row.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","),
       },
       {
-        name: <Stack justifyContent="center" alignItems="center" sx={{width:'100%'}}><Typography variant="body2">플랫폼</Typography></Stack>,
+        name: (
+          <Stack
+            justifyContent="center"
+            alignItems="center"
+            sx={{ width: "100%" }}
+          >
+            <Typography variant="body2">플랫폼</Typography>
+          </Stack>
+        ),
         width: "500px",
         // sortable: true,
         // selector: (row: any) => row.currentTurn+" / "+row.pltfVal,
-        selector: (row: any) => "MTP / "+row.pltfVal,
+        selector: (row: any) => "MTP / " + row.pltfVal,
       },
     ],
-    []
+    [],
   );
-
-  const changeYear = (yearValue: string) => {
-    console.log(yearValue)
-    setYear(yearValue);
-  };
-  const changeMonth = (monthValue: string) => {
-    console.log(monthValue)
-    setMonth(monthValue);
-  };
 
   const subHeaderComponentMemo = useMemo(() => {
     return (
       <Grid container>
-        <Grid item xs={12} sx={{mt: 0}}>
+        <Grid item xs={12} sx={{ mt: 0 }}>
           <Stack
             direction="row"
             justifyContent="space-between"
@@ -173,11 +240,7 @@ const LicenseList = () => {
             sx={{ mb: 0.5 }}
             alignItems="center"
           >
-            <Stack
-              direction="row"
-              alignItems="center"
-              spacing={1}
-            >
+            <Stack direction="row" alignItems="center" spacing={1}>
               <DataCountResultInfo totalCount={totalElements} />
 
               <Stack
@@ -187,15 +250,10 @@ const LicenseList = () => {
                 sx={{ mb: 0.5 }}
                 alignItems="center"
               >
-                <YearMonthSelectBox year={year} month={month} changeYear={changeYear} changeMonth={changeMonth}/>
+                <YearMonthSelectBox />
               </Stack>
-
             </Stack>
-            <Stack
-              direction="row"
-              spacing={1}
-              alignItems="center"
-            >
+            <Stack direction="row" spacing={1} alignItems="center">
               <FileDownloadBtn
                 exportUrl={`/agnc/license/list/download${result}&year=${year}&month=${month}`}
                 iconName="xls3"
@@ -205,28 +263,12 @@ const LicenseList = () => {
             </Stack>
           </Stack>
 
-          <TotalPriceBox year={year} month={month} monthTotalPrice={monthTotalPrice} yearTotalPrice={yearTotalPrice} />
-
-          {/*<TableContainer sx={{ mb: 1, mt:1 }}>*/}
-          {/*  <Table>*/}
-          {/*    <TableBody>*/}
-          {/*      <TableRow>*/}
-          {/*        <TH sx={{ width: "20%" }}>*/}
-          {/*          <Box component="span" sx={{color: cjbsTheme.palette.primary.main}}>{month}월</Box> 사용금액 총계*/}
-          {/*        </TH>*/}
-          {/*        <TD sx={{ width: "30%", textAlign: "left" }}>*/}
-          {/*          {monthTotalPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}*/}
-          {/*        </TD>*/}
-          {/*        <TH sx={{ width: "20%" }}>*/}
-          {/*          <Box component="span" sx={{color: cjbsTheme.palette.primary.main}}>{year}년</Box> 사용금액 총계*/}
-          {/*        </TH>*/}
-          {/*        <TD sx={{ width: "30%", textAlign: "left" }}>*/}
-          {/*          {yearTotalPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}*/}
-          {/*        </TD>*/}
-          {/*      </TableRow>*/}
-          {/*    </TableBody>*/}
-          {/*  </Table>*/}
-          {/*</TableContainer>*/}
+          <TotalPriceBox
+            year={year}
+            month={month}
+            monthTotalPrice={monthTotalPrice}
+            yearTotalPrice={yearTotalPrice}
+          />
         </Grid>
       </Grid>
     );
@@ -250,7 +292,7 @@ const LicenseList = () => {
     // currently, props that extend ExpanderComponentProps must be set to optional.
     // someTitleProp?: string;
   }
-  const ExpandableRowComponent: React.FC<Props> = ({data}) => {
+  const ExpandableRowComponent: React.FC<Props> = ({ data }) => {
     // console.log(">>>>>>>>>>>", data);
     return (
       <>
@@ -258,7 +300,7 @@ const LicenseList = () => {
           <Table aria-label="simple table" size="small">
             <TableBody>
               <TableRow>
-                {data.lcnsCosts.map((item:any, index:number) => (
+                {data.lcnsCosts.map((item: any, index: number) => (
                   <TableCell
                     key={index}
                     align="center"
@@ -272,7 +314,7 @@ const LicenseList = () => {
                 ))}
               </TableRow>
               <TableRow>
-                {data.lcnsCosts.map((item:any, index:number) => (
+                {data.lcnsCosts.map((item: any, index: number) => (
                   <TableCell
                     key={index}
                     align="center"
@@ -280,8 +322,12 @@ const LicenseList = () => {
                       border: `1px solid ${cjbsTheme.palette.grey["400"]}`,
                       backgroundColor: blue["50"],
                     }}
-                >
-                    <Typography variant="body2">{item.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</Typography>
+                  >
+                    <Typography variant="body2">
+                      {item.price
+                        .toString()
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                    </Typography>
                   </TableCell>
                 ))}
               </TableRow>
@@ -312,7 +358,10 @@ const LicenseList = () => {
         noDataComponent={<NoDataView />}
         expandableRows
         expandableRowsComponent={ExpandableRowComponent}
-        expandableIcon={{ collapsed: <MyIcon icon="plus" size={16} />, expanded: <MyIcon icon="minus" size={16} />}}
+        expandableIcon={{
+          collapsed: <MyIcon icon="plus" size={16} />,
+          expanded: <MyIcon icon="minus" size={16} />,
+        }}
         // expandOnRowDoubleClicked={true}
       />
     </Box>
