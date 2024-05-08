2bec86bd6d848cc9275baa82343dd741ee56073d
Mason <choimusong0529@gmail.com>
2024-05-08 14:28:03 +0900

    고객 별 결제 현황 라이선스 탭 -> 년/월 최신화 이슈 수정

--- apps/bsadmin/src/app/(pages)/(ledger)/(cp)/(cust-pay)/ledger-cust-pay-list/[...slug]/CustPayInfo.tsx
+++ apps/bsadmin/src/app/(pages)/(ledger)/(cp)/(cust-pay)/ledger-cust-pay-list/[...slug]/CustPayInfo.tsx
@@ -1,10 +1,12 @@
 "use client";
 import * as React from "react";
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 
 import {
   Box,
-  BoxProps, Grid, IconButton,
+  BoxProps,
+  Grid,
+  IconButton,
   InputAdornment,
   Stack,
   styled,
@@ -18,7 +20,8 @@ import {
   cjbsTheme,
   ContainedButton,
   Form,
-  InputValidation, LinkButton,
+  InputValidation,
+  LinkButton,
   OutlinedButton,
   TD,
   TH,
@@ -43,29 +46,44 @@ const LazyPrePayListModal = dynamic(() => import("./PrePayListModal"), {
 });
 
 const CustPayInfo = () => {
-
   const router = useRouter();
   const params = useParams();
   const anlsItstUkey = params.slug;
 
   const [settlement, setSettlement] = useState<boolean>(true);
   const [selectSampleListData, setSelectSampleListData] = useState<any>({});
-  const [isEdit, setIsEdit] = useState<string>('Y');
+  const [isEdit, setIsEdit] = useState<string>("Y");
   const [prePayList, setPrePayList] = useState<any>([]);
   // [거래처(PI)] 모달
   const [showAgncInfoModal, setShowAgncInfoModal] = useState<boolean>(false);
   // [연구책임자] 모달
-  const [showRearchInfoModal, setShowRearchInfoModal] = useState<boolean>(false);
+  const [showRearchInfoModal, setShowRearchInfoModal] =
+    useState<boolean>(false);
   // [선결제 정산내역] 모달
-  const [showPrePayListModal, setShowPrePayListModal] = useState<boolean>(false);
+  const [showPrePayListModal, setShowPrePayListModal] =
+    useState<boolean>(false);
   const [year, setYear] = useState<string>("2024");
 
-  const { data } = useSWR(`/agnc/${anlsItstUkey}/pymt/detail?year=${year}`, fetcher, {
-    suspense: true,
-  });
+  const { data } = useSWR(
+    `/agnc/${anlsItstUkey}/pymt/detail?year=${year}`,
+    fetcher,
+    {
+      suspense: true,
+    },
+  );
   console.log("response", data);
   const { pymtStatusList } = data;
-  const { agncUkey, agncNm, bsnsMngrNm, instNm, pymtStatusVal, rhpiNm, rmnPrice, totalAnlsPrice, totalPymtPrice } = data.agncPymtInfo;
+  const {
+    agncUkey,
+    agncNm,
+    bsnsMngrNm,
+    instNm,
+    pymtStatusVal,
+    rhpiNm,
+    rmnPrice,
+    totalAnlsPrice,
+    totalPymtPrice,
+  } = data.agncPymtInfo;
 
   // useEffect(() => {
   // }, []);
@@ -93,34 +111,32 @@ const CustPayInfo = () => {
   // };
 
   const changeYear = (yearValue) => {
-    console.log(yearValue)
+    console.log(yearValue);
     setYear(yearValue);
   };
 
   return (
     <>
-      <Box sx={{mb: 4}}>
-        <Title1 titleName="고객 결제 정보"/>
+      <Box sx={{ mb: 4 }}>
+        <Title1 titleName="거래처 결제 정보" />
       </Box>
       <Typography variant="subtitle1">고객정보</Typography>
-      <TableContainer sx={{mb: 5}}>
+      <TableContainer sx={{ mb: 5 }}>
         <Table>
           <TableBody>
             <TableRow>
               {/*const { agncNm, bsnsMngrNm, instNm, pymtStatusVal, rhpiNm, rmnPrice, totalAnlsPrice, totalPymtPrice } = data.agncPymtInfo;*/}
-              <TH sx={{width: "15%"}}>거래처(PI)</TH>
-              <TD sx={{width: "35%"}}>
+              <TH sx={{ width: "15%" }}>거래처(PI)</TH>
+              <TD sx={{ width: "35%" }}>
                 <Grid container justifyContent="space-between">
                   <Grid item item xs={10}>
                     {agncNm}
                   </Grid>
-                  <Grid item>
-                    {/*<AgncDetailInfo agncUkey={agncUkey} />*/}
-                  </Grid>
+                  <Grid item>{/*<AgncDetailInfo agncUkey={agncUkey} />*/}</Grid>
                 </Grid>
               </TD>
-              <TH sx={{width: "15%"}}>영업 담당자</TH>
-              <TD sx={{width: "35%"}}>
+              <TH sx={{ width: "15%" }}>영업 담당자</TH>
+              <TD sx={{ width: "35%" }}>
                 <Grid container justifyContent="space-between">
                   <Grid item xs={10}>
                     {bsnsMngrNm}
@@ -129,26 +145,27 @@ const CustPayInfo = () => {
                     {/*<RearchDetailInfo agncLeaderUkey={custUkey} />*/}
                   </Grid>
                 </Grid>
-
               </TD>
             </TableRow>
             <TableRow>
-              <TH sx={{width: "15%"}}>결제 총계</TH>
-              <TD sx={{width: "35%"}}>
-                {totalPymtPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+              <TH sx={{ width: "15%" }}>결제 총계</TH>
+              <TD sx={{ width: "35%" }}>
+                {totalPymtPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
               </TD>
-              <TH sx={{width: "15%"}}>분석 총계</TH>
-              <TD sx={{width: "35%"}}>
-                {totalAnlsPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+              <TH sx={{ width: "15%" }}>분석 총계</TH>
+              <TD sx={{ width: "35%" }}>
+                {totalAnlsPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
               </TD>
             </TableRow>
             <TableRow>
-              <TH sx={{width: "15%"}}>상태</TH>
-              <TD sx={{width: "35%"}}>
-                {pymtStatusVal}
-              </TD>
-              <TH sx={{width: "15%"}}>남은 금액</TH>
-              <TD sx={{width: "35%"}}>
+              <TH sx={{ width: "15%" }}>상태</TH>
+              <TD sx={{ width: "35%" }}>{pymtStatusVal}</TD>
+              <TH sx={{ width: "15%" }}>남은 금액</TH>
+              <TD sx={{ width: "35%" }}>
                 {rmnPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
               </TD>
             </TableRow>
@@ -156,51 +173,186 @@ const CustPayInfo = () => {
         </Table>
       </TableContainer>
 
-      <Stack direction="row" spacing={1} justifyContent="space-between" sx={{mb: 1}}>
+      <Stack
+        direction="row"
+        spacing={1}
+        justifyContent="space-between"
+        sx={{ mb: 1 }}
+      >
         <Typography variant="subtitle1">결제 현황</Typography>
         <YearSelectBox changeYear={changeYear} />
       </Stack>
 
-      <TableContainer sx={{mb: 5}}>
+      <TableContainer sx={{ mb: 5 }}>
         <Table>
           <TableBody>
             <TableRow>
-              <TH sx={{width: "9%", backgroundColor: cjbsTheme.palette.grey["700"], color: '#ffffff'}} align='center'>구분</TH>
-              <TH sx={{width: "7%", backgroundColor: cjbsTheme.palette.grey["700"], color: '#ffffff'}} align='center'>1월</TH>
-              <TH sx={{width: "7%", backgroundColor: cjbsTheme.palette.grey["700"], color: '#ffffff'}} align='center'>2월</TH>
-              <TH sx={{width: "7%", backgroundColor: cjbsTheme.palette.grey["700"], color: '#ffffff'}} align='center'>3월</TH>
-              <TH sx={{width: "7%", backgroundColor: cjbsTheme.palette.grey["700"], color: '#ffffff'}} align='center'>4월</TH>
-              <TH sx={{width: "7%", backgroundColor: cjbsTheme.palette.grey["700"], color: '#ffffff'}} align='center'>5월</TH>
-              <TH sx={{width: "7%", backgroundColor: cjbsTheme.palette.grey["700"], color: '#ffffff'}} align='center'>6월</TH>
-              <TH sx={{width: "7%", backgroundColor: cjbsTheme.palette.grey["700"], color: '#ffffff'}} align='center'>7월</TH>
-              <TH sx={{width: "7%", backgroundColor: cjbsTheme.palette.grey["700"], color: '#ffffff'}} align='center'>8월</TH>
-              <TH sx={{width: "7%", backgroundColor: cjbsTheme.palette.grey["700"], color: '#ffffff'}} align='center'>9월</TH>
-              <TH sx={{width: "7%", backgroundColor: cjbsTheme.palette.grey["700"], color: '#ffffff'}} align='center'>10월</TH>
-              <TH sx={{width: "7%", backgroundColor: cjbsTheme.palette.grey["700"], color: '#ffffff'}} align='center'>11월</TH>
-              <TH sx={{width: "7%", backgroundColor: cjbsTheme.palette.grey["700"], color: '#ffffff'}} align='center'>12월</TH>
-              <TH sx={{width: "7%", backgroundColor: cjbsTheme.palette.grey["700"], color: '#ffffff'}} align='center'>합계</TH>
+              <TH
+                sx={{
+                  width: "9%",
+                  backgroundColor: cjbsTheme.palette.grey["700"],
+                  color: "#ffffff",
+                }}
+                align="center"
+              >
+                구분
+              </TH>
+              <TH
+                sx={{
+                  width: "7%",
+                  backgroundColor: cjbsTheme.palette.grey["700"],
+                  color: "#ffffff",
+                }}
+                align="center"
+              >
+                1월
+              </TH>
+              <TH
+                sx={{
+                  width: "7%",
+                  backgroundColor: cjbsTheme.palette.grey["700"],
+                  color: "#ffffff",
+                }}
+                align="center"
+              >
+                2월
+              </TH>
+              <TH
+                sx={{
+                  width: "7%",
+                  backgroundColor: cjbsTheme.palette.grey["700"],
+                  color: "#ffffff",
+                }}
+                align="center"
+              >
+                3월
+              </TH>
+              <TH
+                sx={{
+                  width: "7%",
+                  backgroundColor: cjbsTheme.palette.grey["700"],
+                  color: "#ffffff",
+                }}
+                align="center"
+              >
+                4월
+              </TH>
+              <TH
+                sx={{
+                  width: "7%",
+                  backgroundColor: cjbsTheme.palette.grey["700"],
+                  color: "#ffffff",
+                }}
+                align="center"
+              >
+                5월
+              </TH>
+              <TH
+                sx={{
+                  width: "7%",
+                  backgroundColor: cjbsTheme.palette.grey["700"],
+                  color: "#ffffff",
+                }}
+                align="center"
+              >
+                6월
+              </TH>
+              <TH
+                sx={{
+                  width: "7%",
+                  backgroundColor: cjbsTheme.palette.grey["700"],
+                  color: "#ffffff",
+                }}
+                align="center"
+              >
+                7월
+              </TH>
+              <TH
+                sx={{
+                  width: "7%",
+                  backgroundColor: cjbsTheme.palette.grey["700"],
+                  color: "#ffffff",
+                }}
+                align="center"
+              >
+                8월
+              </TH>
+              <TH
+                sx={{
+                  width: "7%",
+                  backgroundColor: cjbsTheme.palette.grey["700"],
+                  color: "#ffffff",
+                }}
+                align="center"
+              >
+                9월
+              </TH>
+              <TH
+                sx={{
+                  width: "7%",
+                  backgroundColor: cjbsTheme.palette.grey["700"],
+                  color: "#ffffff",
+                }}
+                align="center"
+              >
+                10월
+              </TH>
+              <TH
+                sx={{
+                  width: "7%",
+                  backgroundColor: cjbsTheme.palette.grey["700"],
+                  color: "#ffffff",
+                }}
+                align="center"
+              >
+                11월
+              </TH>
+              <TH
+                sx={{
+                  width: "7%",
+                  backgroundColor: cjbsTheme.palette.grey["700"],
+                  color: "#ffffff",
+                }}
+                align="center"
+              >
+                12월
+              </TH>
+              <TH
+                sx={{
+                  width: "7%",
+                  backgroundColor: cjbsTheme.palette.grey["700"],
+                  color: "#ffffff",
+                }}
+                align="center"
+              >
+                합계
+              </TH>
             </TableRow>
             <TableRow>
-              <TH sx={{width: "9%"}} align='center'>분석비용</TH>
-              { pymtStatusList.map((item: any, index: any) => {
-                const {
-                  anlsPrice,
-                  prcsPymtPrice,
-                } = item;
+              <TH sx={{ width: "9%" }} align="center">
+                분석비용
+              </TH>
+              {pymtStatusList.map((item: any, index: any) => {
+                const { anlsPrice, prcsPymtPrice } = item;
                 return (
-                  <TD key={index} sx={{width: "7%"}} align='center'>{anlsPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</TD>
+                  <TD key={index} sx={{ width: "7%" }} align="center">
+                    {anlsPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                  </TD>
                 );
               })}
             </TableRow>
             <TableRow>
-              <TH sx={{width: "9%"}} align='center'>계산서 처리</TH>
-              { pymtStatusList.map((item: any, index: any) => {
-                const {
-                  anlsPrice,
-                  prcsPymtPrice,
-                } = item;
+              <TH sx={{ width: "9%" }} align="center">
+                계산서 처리
+              </TH>
+              {pymtStatusList.map((item: any, index: any) => {
+                const { anlsPrice, prcsPymtPrice } = item;
                 return (
-                  <TD key={index} sx={{width: "7%"}} align='center'>{prcsPymtPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</TD>
+                  <TD key={index} sx={{ width: "7%" }} align="center">
+                    {prcsPymtPrice
+                      .toString()
+                      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                  </TD>
                 );
               })}
             </TableRow>
@@ -208,11 +360,11 @@ const CustPayInfo = () => {
         </Table>
       </TableContainer>
 
-      <ListPymtPrice agncUkey={agncUkey}/>
+      <ListPymtPrice agncUkey={agncUkey} />
 
-      <Stack direction="row" spacing={1} justifyContent="center" sx={{mb: 5}}>
+      <Stack direction="row" spacing={1} justifyContent="center" sx={{ mb: 5 }}>
         <Link href="/ledger-cust-pay-list">
-          <OutlinedButton size="medium" buttonName="목록"/>
+          <OutlinedButton size="medium" buttonName="목록" />
         </Link>
       </Stack>
 
