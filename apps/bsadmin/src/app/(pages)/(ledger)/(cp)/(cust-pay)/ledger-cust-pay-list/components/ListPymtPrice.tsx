6012fde90bc2487f9b24701c6ed775071edab02b
mason <choimusong0529@gmail.com>
2024-05-09 10:26:07 +0900

    [장부] 결제 처리 내역 API, UI 변경

--- apps/bsadmin/src/app/(pages)/(ledger)/(cp)/(cust-pay)/ledger-cust-pay-list/components/ListPymtPrice.tsx
+++ apps/bsadmin/src/app/(pages)/(ledger)/(cp)/(cust-pay)/ledger-cust-pay-list/components/ListPymtPrice.tsx
@@ -2,23 +2,32 @@
 import * as React from "react";
 import { useMemo } from "react";
 import {
-    DataCountResultInfo,
-    DataTableBase,
-    Title1,
-    ContainedButton,
-    cjbsTheme,
-    FileDownloadBtn, green, red, grey, TH, TD,
+  DataCountResultInfo,
+  DataTableBase,
+  Title1,
+  ContainedButton,
+  cjbsTheme,
+  FileDownloadBtn,
+  green,
+  red,
+  grey,
+  TH,
+  TD,
+  formatNumberWithCommas,
 } from "cjbsDSTM";
+import { blue } from "cjbsDSTM/themes/color";
 import {
-  blue,
-} from "cjbsDSTM/themes/color";
-import {
-    Box,
-    Stack,
-    Grid,
-    Typography,
-    Collapse,
-    TableContainer, Table, TableBody, TableCell, TableRow, Chip,
+  Box,
+  Stack,
+  Grid,
+  Typography,
+  Collapse,
+  TableContainer,
+  Table,
+  TableBody,
+  TableCell,
+  TableRow,
+  Chip,
 } from "@mui/material";
 import { useRouter } from "next-nprogress-bar";
 import { useState } from "react";
@@ -31,6 +40,7 @@ import KeywordSearch from "../../../../../../components/KeywordSearch";
 import NoDataView from "../../../../../../components/NoDataView";
 import dynamic from "next/dynamic";
 import { ExpanderComponentProps } from "react-data-table-component";
+import Link from "next/link";
 
 const ListPymtPrice = (props: any) => {
   const agncUkey = props.agncUkey;
@@ -48,18 +58,24 @@ const ListPymtPrice = (props: any) => {
   const result = "?" + new URLSearchParams(resultObject).toString();
   console.log("RESULT@#@#@#", JSON.stringify(result));
 
+  // /agnc/{agncUkey}/pymt/list
+
   const { data } = useSWR(
     JSON.stringify(resultObject) !== "{}"
-      ? `/agnc/${agncUkey}/prePymtPrice/list${result}&page=${page}&size=${size}`
-      : `/agnc/${agncUkey}/prePymtPrice/list?page=${page}&size=${size}`,
+      ? `/agnc/${agncUkey}/pymt/list${result}&page=${page}&size=${size}`
+      : `/agnc/${agncUkey}/pymt/list?page=${page}&size=${size}`,
     fetcher,
     {
       suspense: true,
-    }
+    },
   );
-  console.log("RUN LIST DATA", data);
+  console.log("PAYMENT LIST DATA", data);
+
+  const { agncPymtList, pageInfo } = data;
+  const { totalElements } = pageInfo;
+
   const prePymtPrcsHstrResList = data.prePymtPrcsHstrResList;
-  const totalElements = data.pageInfo.totalElements;
+  // const totalElements = data.pageInfo.totalElements;
   const rmnPrePymtPrice = data.rmnPrePymtPrice;
   const price = data.price;
   const prePymtPrice = data.prePymtPrice;
@@ -68,64 +84,71 @@ const ListPymtPrice = (props: any) => {
   const router = useRouter();
   const [resetPaginationToggle, setResetPaginationToggle] = useState(false);
 
-
   const columns = useMemo(
     () => [
       {
-        name: "",
-        width: "5%",
-        center: true,
-        // sortable: true,
-        selector: (row, index) => row.invcId,
+        name: "처리일",
+        selector: (row) => row.prcsDttm,
       },
       {
-        name: "발행일자",
-        width: "14.5%",
-        center: true,
-        // sortable: true,
-        selector: (row, index) => row.issuDttm,
+        name: "구분",
+        selector: (row) => row.costType,
       },
       {
-        name: "선결제 금액",
-        width: "19.4%",
-        center: true,
-        // sortable: true,
-        selector: (row, index) => row.prePymtPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+        name: "상세 구분",
+        selector: (row) => row.costTypeDetail,
       },
       {
-        name: "사용일(분석일)",
-        width: "19.4%",
-        center: true,
-        // sortable: true,
-        selector: (row, index) => row.prcsDate,
-      },
-      {
-        name: "사용 금액",
-        width: "19.4%",
-        center: true,
-        // sortable: true,
-        selector: (row, index) => row.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","),
-      },
-      {
-        name: "남은 선결제 금액",
-        width: "19.4%",
-        center: true,
-        // sortable: true,
-        cell: (row: { rmnPrePymtPrice: string }) => {
-          const { rmnPrePymtPrice } = row;
+        name: "금액",
+        right: true,
+        selector: (row) => formatNumberWithCommas(row.price),
+        cell: (row) => {
           return (
-              <Typography variant="body2" sx={{color: "#EF151E"}}>{rmnPrePymtPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</Typography>
+            <Stack
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+              spacing={0.5}
+            >
+              <Typography variant="body2">
+                {formatNumberWithCommas(row.price)}
+              </Typography>
+              <Typography variant="body2">원</Typography>
+            </Stack>
+          );
+        },
+      },
+      {
+        name: "번호",
+        width: "100px",
+        right: true,
+        // sortable: true,
+        selector: (row) => row.anlsItstId,
+        cell: (row) => {
+          // /ledger-license-report-list/ETqJma
+          const { anlsItstId, anlsItstUkey, invcId, invcUkey } = row;
+          const path =
+            anlsItstId !== null
+              ? `/ledger-license-report-list/${anlsItstUkey}`
+              : `/ledger-tax-invoice-list/${invcUkey}`;
+          const id = anlsItstId !== null ? anlsItstId : invcId;
+          return (
+            <Link href={path}>
+              <Typography variant="body2" color="primary">
+                {id}
+              </Typography>
+            </Link>
           );
         },
       },
     ],
-    []
+    [],
   );
 
   const subHeaderComponentMemo = useMemo(() => {
     return (
       <Grid container>
-        <Grid item xs={12} sx={{mt: 0}}>
+        <Grid item xs={12} sx={{ mt: 0 }}>
           <TableContainer>
             <Table>
               <TableBody>
@@ -133,16 +156,18 @@ const ListPymtPrice = (props: any) => {
                   <TH sx={{ minWidth: "48px", textAlign: "center" }}></TH>
                   <TH sx={{ width: "20%", textAlign: "center" }}>합계</TH>
                   <TD sx={{ width: "20%", textAlign: "center" }}>
-                    {prePymtPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                    {/*{prePymtPrice*/}
+                    {/*  .toString()*/}
+                    {/*  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}*/}
                   </TD>
+                  <TD sx={{ width: "20%", textAlign: "center" }}>-</TD>
                   <TD sx={{ width: "20%", textAlign: "center" }}>
-                    -
-                  </TD>
-                  <TD sx={{ width: "20%", textAlign: "center" }}>
-                    {price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                    {/*{price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}*/}
                   </TD>
                   <TD sx={{ width: "20%", textAlign: "center", color: "red" }}>
-                    {rmnPrePymtPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                    {/*{rmnPrePymtPrice*/}
+                    {/*  .toString()*/}
+                    {/*  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}*/}
                   </TD>
                 </TableRow>
               </TableBody>
@@ -171,60 +196,84 @@ const ListPymtPrice = (props: any) => {
     // currently, props that extend ExpanderComponentProps must be set to optional.
     // someTitleProp?: string;
   }
-  const ExpandableRowComponent: React.FC<Props> = ({data}) => {
-    // console.log(">>>>>>>>>>>", data);
-    return (
-      <>
-        <TableContainer>
-          <Table aria-label="simple table" size="small">
-            <TableBody>
-              {data.prePymtPrcsHstrList.map((item:any, index:number) => (
-                <TableRow
-                  key={index}
-                  sx={{
-                    border: `1px solid ${cjbsTheme.palette.grey["400"]}`,
-                    backgroundColor: blue["50"],
-                  }}
-                >
-                  <TableCell sx={{minWidth:'48px'}}>
-                  </TableCell>
-                  <TableCell width={'40%'} align="right">
-                    <Typography variant="body2">{item.prcsTypeVal}</Typography>
-                  </TableCell>
-                  <TableCell width={'20%'} align="center" sx={{borderLeft: `1px solid ${cjbsTheme.palette.grey["400"]}`}}>
-                    <Typography variant="body2">{item.prcsDate}</Typography>
-                  </TableCell>
-                  <TableCell width={'20%'} align="center" sx={{borderLeft: `1px solid ${cjbsTheme.palette.grey["400"]}`}}>
-                    <Typography variant="body2">{item.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</Typography>
-                  </TableCell>
-                  <TableCell width={'20%'} align="center" sx={{borderLeft: `1px solid ${cjbsTheme.palette.grey["400"]}`}}>
-                    <Typography variant="body2">{item.rmnPrePymtPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</Typography>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </>
-    );
-  };
+  // const ExpandableRowComponent: React.FC<Props> = ({ data }) => {
+  //   return (
+  //     <>
+  //       <TableContainer>
+  //         <Table aria-label="simple table" size="small">
+  //           <TableBody>
+  //             {data.prePymtPrcsHstrList.map((item: any, index: number) => (
+  //               <TableRow
+  //                 key={index}
+  //                 sx={{
+  //                   border: `1px solid ${cjbsTheme.palette.grey["400"]}`,
+  //                   backgroundColor: blue["50"],
+  //                 }}
+  //               >
+  //                 <TableCell sx={{ minWidth: "48px" }}></TableCell>
+  //                 <TableCell width={"40%"} align="right">
+  //                   <Typography variant="body2">{item.prcsTypeVal}</Typography>
+  //                 </TableCell>
+  //                 <TableCell
+  //                   width={"20%"}
+  //                   align="center"
+  //                   sx={{
+  //                     borderLeft: `1px solid ${cjbsTheme.palette.grey["400"]}`,
+  //                   }}
+  //                 >
+  //                   <Typography variant="body2">{item.prcsDate}</Typography>
+  //                 </TableCell>
+  //                 <TableCell
+  //                   width={"20%"}
+  //                   align="center"
+  //                   sx={{
+  //                     borderLeft: `1px solid ${cjbsTheme.palette.grey["400"]}`,
+  //                   }}
+  //                 >
+  //                   <Typography variant="body2">
+  //                     {item.price
+  //                       .toString()
+  //                       .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+  //                   </Typography>
+  //                 </TableCell>
+  //                 <TableCell
+  //                   width={"20%"}
+  //                   align="center"
+  //                   sx={{
+  //                     borderLeft: `1px solid ${cjbsTheme.palette.grey["400"]}`,
+  //                   }}
+  //                 >
+  //                   <Typography variant="body2">
+  //                     {item.rmnPrePymtPrice
+  //                       .toString()
+  //                       .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+  //                   </Typography>
+  //                 </TableCell>
+  //               </TableRow>
+  //             ))}
+  //           </TableBody>
+  //         </Table>
+  //       </TableContainer>
+  //     </>
+  //   );
+  // };
 
   return (
     <>
       <DataTableBase
         title={
           <Stack direction="row" spacing={2} sx={{ mb: 0 }}>
-            <Typography variant="subtitle1">선결제 처리 내역</Typography>
+            <Typography variant="subtitle1">결제 처리 내역</Typography>
           </Stack>
         }
-        data={prePymtPrcsHstrResList}
+        data={agncPymtList}
         columns={columns}
         // onRowClicked={goDetailPage}
-        pointerOnHover
-        highlightOnHover
+        // pointerOnHover
+        // highlightOnHover
         customStyles={dataTableCustomStyles3}
-        subHeader
-        subHeaderComponent={subHeaderComponentMemo}
+        // subHeader
+        // subHeaderComponent={subHeaderComponentMemo}
         paginationResetDefaultPage={resetPaginationToggle}
         selectableRows={false}
         pagination
@@ -233,9 +282,12 @@ const ListPymtPrice = (props: any) => {
         onChangeRowsPerPage={handlePerRowsChange}
         onChangePage={handlePageChange}
         noDataComponent={<NoDataView />}
-        expandableRows
-        expandableRowsComponent={ExpandableRowComponent}
-        expandableIcon={{ collapsed: <MyIcon icon="plus" size={16} />, expanded: <MyIcon icon="minus" size={16} />}}
+        // expandableRows
+        // expandableRowsComponent={ExpandableRowComponent}
+        // expandableIcon={{
+        //   collapsed: <MyIcon icon="plus" size={16} />,
+        //   expanded: <MyIcon icon="minus" size={16} />,
+        // }}
         // expandOnRowDoubleClicked={true}
       />
     </>
