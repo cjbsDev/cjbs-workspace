2feb760b3cfb4b65250397d75926f2ea5f9dceae
mason <choimusong0529@gmail.com>
2024-06-11 15:22:12 +0900

    비밀번호찾기 추가

--- apps/bsadmin/src/app/(pages)/(auth)/password-reset/page.tsx
+++ apps/bsadmin/src/app/(pages)/(auth)/password-reset/page.tsx
@@ -11,12 +11,13 @@ import {
   Typography,
 } from "@mui/material";
 import MyIcon from "icon/MyIcon";
-import { Form, InputValidation } from "cjbsDSTM";
+import { ContainedButton, Form, InputValidation } from "cjbsDSTM";
 import { LoadingButton } from "@mui/lab";
 import * as React from "react";
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { useRouter } from "next-nprogress-bar";
+import Link from "next/link";
 
 export default function Page() {
   const router = useRouter();
@@ -25,7 +26,8 @@ export default function Page() {
   const key = encodeURIComponent(rawKey).replace("%20", "+");
 
   const [keyChck, setKeyChck] = useState<boolean>(true);
-  const [keyChckMsg, setKeyChckMsg] = useState(null);
+  const [keyChckMsg, setKeyChckMsg] = useState("Key값을 확인중 입니다.");
+  const [keyChckCde, setKeyChckCde] = useState("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [showPassword, setShowPassword] = React.useState(false);
   const handleClickShowPassword = () => setShowPassword((show) => !show);
@@ -35,7 +37,7 @@ export default function Page() {
     event.preventDefault();
   };
 
-  console.log("key", decodeURIComponent(key));
+  // console.log("key", decodeURIComponent(key));
 
   useEffect(() => {
     axios
@@ -47,10 +49,10 @@ export default function Page() {
         console.log(response);
         if (response.data.success) {
           setKeyChck(false);
-          // router.push("/password-reset-success");
         } else {
           // toast(response.data.message);
           setKeyChckMsg(response.data.message);
+          setKeyChckCde(response.data.code);
           setKeyChck(true);
         }
       })
@@ -90,6 +92,10 @@ export default function Page() {
       });
   };
 
+  // if (keyChck) {
+  //   return <div>{keyChckMsg}</div>;
+  // }
+
   return (
     <Container
       component="main"
@@ -108,109 +114,135 @@ export default function Page() {
         <Typography variant="h4" sx={{ marginBottom: 2 }}>
           비밀번호 재설정
         </Typography>
-        <Typography
-          variant="body2"
-          sx={{ marginBottom: 2, textAlign: "center" }}
-        >
-          다른 아이디나 사이트에서 사용한 적 없는 안전한
-          <br />
-          비밀번호로 변경해 주세요.
-          <br />6 자리 이상 영문, 숫자, 특수기호를 사용하여 비밀번호 생성.
-        </Typography>
 
-        <Form onSubmit={onSubmit} defaultValues={undefined}>
-          <InputValidation
-            margin="normal"
-            inputName="password"
-            placeholder="새 비밀번호"
-            required={true}
-            errorMessage="비밀번호를 입력해 주세요."
-            pattern={
-              /^(?=.*[A-Za-z])(?=.*\d)(?=.*[~!@#$%^&*_+=()-])[A-Za-z\d~!@#$%^&*_+=()-]{6,}$/
-            }
-            patternErrMsg="6자리 이상 영문, 숫자, 특수기호를 사용해 주세요."
-            type={showPassword ? "text" : "password"}
-            sx={{ width: 380 }}
-            inputProps={{
-              style: {
-                height: 39.6,
-              },
-            }}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <MyIcon icon="lock" size={20} />
-                </InputAdornment>
-              ),
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                  >
-                    {showPassword ? (
-                      <MyIcon icon="eye-slash-fill" size={24} />
-                    ) : (
-                      <MyIcon icon="eye-fill" size={24} />
-                    )}
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
-          />
+        {keyChck ? (
+          <>
+            <Alert
+              severity={
+                keyChckMsg === "Key값을 확인중 입니다." ? "info" : "warning"
+              }
+              sx={{ mb: 3 }}
+            >
+              {keyChckMsg}
+            </Alert>
+            {keyChckCde === "USER_VERIFICATION_ALREADY" ? (
+              <Link href="/sign-in">
+                <ContainedButton size="small" buttonName="로그인으로 이동" />
+              </Link>
+            ) : keyChckCde === "USER_VERIFICATION_EXPIRE" ? (
+              <Link href="/password-find">
+                <ContainedButton size="small" buttonName="비밀번호찾기 이동" />
+              </Link>
+            ) : keyChckCde === "USER_VERIFICATION_NOT_EXIST" ? (
+              <Link href="/password-find">
+                <ContainedButton size="small" buttonName="비밀번호찾기 이동" />
+              </Link>
+            ) : null}
+          </>
+        ) : (
+          <>
+            <Typography
+              variant="body2"
+              sx={{ marginBottom: 2, textAlign: "center" }}
+            >
+              다른 아이디나 사이트에서 사용한 적 없는 안전한
+              <br />
+              비밀번호로 변경해 주세요.
+              <br />6 자리 이상 영문, 숫자, 특수기호를 사용하여 비밀번호 생성.
+            </Typography>
 
-          <InputValidation
-            inputName="passwordChck"
-            placeholder="새 비밀번호 확인"
-            required={true}
-            errorMessage="비밀번호를 입력해 주세요."
-            type={showPassword ? "text" : "password"}
-            sx={{ width: 380 }}
-            inputProps={{
-              style: {
-                height: 39.6,
-              },
-            }}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <MyIcon icon="lock" size={20} />
-                </InputAdornment>
-              ),
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                  >
-                    {showPassword ? (
-                      <MyIcon icon="eye-slash-fill" size={24} />
-                    ) : (
-                      <MyIcon icon="eye-fill" size={24} />
-                    )}
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
-          />
+            <Form onSubmit={onSubmit} defaultValues={undefined}>
+              <InputValidation
+                margin="normal"
+                inputName="password"
+                placeholder="새 비밀번호"
+                required={true}
+                errorMessage="비밀번호를 입력해 주세요."
+                pattern={
+                  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[~!@#$%^&*_+=()-])[A-Za-z\d~!@#$%^&*_+=()-]{6,}$/
+                }
+                patternErrMsg="6자리 이상 영문, 숫자, 특수기호를 사용해 주세요."
+                type={showPassword ? "text" : "password"}
+                // sx={{ width: 380 }}
+                inputProps={{
+                  style: {
+                    height: 39.6,
+                  },
+                }}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <MyIcon icon="lock" size={20} />
+                    </InputAdornment>
+                  ),
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                      >
+                        {showPassword ? (
+                          <MyIcon icon="eye-slash-fill" size={24} />
+                        ) : (
+                          <MyIcon icon="eye-fill" size={24} />
+                        )}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
 
-          <LoadingButton
-            loading={isLoading}
-            variant="contained"
-            type="submit"
-            fullWidth
-            size="large"
-            style={{ marginTop: 10, marginBottom: 10 }}
-            disabled={keyChck}
-          >
-            비밀번호 변경
-          </LoadingButton>
-        </Form>
-      </Stack>
-      <Stack>
-        {keyChckMsg !== null && <Alert severity="error">{keyChckMsg}</Alert>}
+              <InputValidation
+                inputName="passwordChck"
+                placeholder="새 비밀번호 확인"
+                required={true}
+                errorMessage="비밀번호를 입력해 주세요."
+                type={showPassword ? "text" : "password"}
+                sx={{ width: 380 }}
+                inputProps={{
+                  style: {
+                    height: 39.6,
+                  },
+                }}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <MyIcon icon="lock" size={20} />
+                    </InputAdornment>
+                  ),
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                      >
+                        {showPassword ? (
+                          <MyIcon icon="eye-slash-fill" size={24} />
+                        ) : (
+                          <MyIcon icon="eye-fill" size={24} />
+                        )}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+
+              <LoadingButton
+                loading={isLoading}
+                variant="contained"
+                type="submit"
+                fullWidth
+                size="large"
+                style={{ marginTop: 10, marginBottom: 10 }}
+                disabled={keyChck}
+              >
+                비밀번호 변경
+              </LoadingButton>
+            </Form>
+          </>
+        )}
       </Stack>
     </Container>
   );
