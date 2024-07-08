4be459af681d0ab3c1945addbddb2484cd5b10ed
mason <choimusong0529@gmail.com>
2024-07-08 12:09:06 +0900

    signout 수정

--- apps/bsadmin/src/app/components/AppBar/index.tsx
+++ apps/bsadmin/src/app/components/AppBar/index.tsx
@@ -156,7 +156,8 @@ const AppBar = ({ open, handleDrawerOpen, handleDrawerClose }: AppBarProps) => {
                   <MenuItem
                     onClick={() => {
                       popupState.close();
-                      signOut({ callbackUrl: "/sign-in" });
+                      // signOut({ callbackUrl: "/sign-in" });
+                      signOut();
                     }}
                   >
                     <Stack direction="row" spacing={1} alignItems="center">
