a3b6adb41bbe4e715838c685abb317cfce9e8100
Mason <choimusong0529@gmail.com>
2024-07-12 12:17:45 +0900

    견적서 이미지 이슈 - 로그 추가

--- apps/bsadmin/src/app/(pages)/(qttn)/orsh-qttn-list/[slug]/components/TSImgPreview.tsx
+++ apps/bsadmin/src/app/(pages)/(qttn)/orsh-qttn-list/[slug]/components/TSImgPreview.tsx
@@ -1,13 +1,9 @@
 import React, { forwardRef } from "react";
-import {
-  SkeletonLoading,
-} from "cjbsDSTM";
-import {
-  Stack,
-} from "@mui/material";
-import Image from 'next/image';
-import {fetcher} from "api";
-import {useParams} from "next/navigation";
+import { SkeletonLoading } from "cjbsDSTM";
+import { Stack } from "@mui/material";
+import Image from "next/image";
+import { fetcher } from "api";
+import { useParams } from "next/navigation";
 import useSWR from "swr";
 import "./print.css";
 
@@ -16,29 +12,32 @@ const TSImgPreview = forwardRef((props, ref) => {
   const { slug } = params;
 
   // data load
-  const { data: imgUrl, error, isLoading, } = useSWR(`/qttn/url/${slug}`, fetcher);
+  const {
+    data: imgUrl,
+    error,
+    isLoading,
+  } = useSWR(`/qttn/url/${slug}`, fetcher);
   if (isLoading) {
     return <SkeletonLoading />;
   }
-  // console.log("imgUrl", imgUrl);
+  console.log("견적서 imgUrl ==>>", imgUrl);
 
   return (
-
     <Stack
       spacing={1}
       alignItems="center"
       justifyContent="start"
       sx={{
-        bgcolor: '#ffffff',
-        width: '773px',
-        height: '1094px',
+        bgcolor: "#ffffff",
+        width: "773px",
+        height: "1094px",
         // border: '1px solid #000000',
         position: "relative",
       }}
       className="printArea"
-      ref={ref}
+      // ref={ref}
     >
-      <Image src={imgUrl} alt={'거래명세서_이미지'} width={773} height={1094}/>
+      <Image src={imgUrl} alt={"거래명세서_이미지"} width={773} height={1094} />
     </Stack>
   );
 });
