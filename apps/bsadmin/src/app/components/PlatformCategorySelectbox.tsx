754694c517bbf9efa3a6f96718c0f3b7d218175c
Mason <choimusong0529@gmail.com>
2024-08-20 10:50:50 +0900

    오더관리 -상세검색: 샘플종류, 플랫폼종류 검색필터 추가 및 UI개선

---
+++ apps/bsadmin/src/app/components/PlatformCategorySelectbox.tsx
@@ -0,0 +1,26 @@
+import { ContainedButton, SelectBox } from "cjbsDSTM";
+import { useFormContext } from "react-hook-form";
+import useSWR from "swr";
+import { fetcher } from "api";
+
+export default function PlatformCategorySelectbox() {
+  const { data } = useSWR(
+    `/code/list/shortly/value?topValue=Platform&midValue=none`,
+    fetcher,
+    {
+      suspense: true,
+    },
+  );
+
+  console.log(data);
+
+  return (
+    <SelectBox
+      // required={true}
+      errorMessage="플랫폼 종류를 선택헤 주세요."
+      inputName="pltfMc"
+      options={data}
+      sx={{ width: "100%" }}
+    />
+  );
+}
