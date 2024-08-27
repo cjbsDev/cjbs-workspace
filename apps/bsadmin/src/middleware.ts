a1fc658c8ff95395daa4fc48bbbcae5c2d371196
Mason <choimusong0529@gmail.com>
2024-08-27 13:26:28 +0900

    redirect오류 수정

--- apps/bsadmin/src/middleware.ts
+++ apps/bsadmin/src/middleware.ts
@@ -34,7 +34,8 @@ export async function middleware(request: NextRequest) {
     rtToken !== undefined &&
     token?.error !== "RefreshAccessTokenError"
   ) {
-    return NextResponse.rewrite(new URL("/", request.url));
+    // return NextResponse.rewrite(new URL(DASHBOARD_PAGE, request.url));
+    return NextResponse.redirect(new URL(DASHBOARD_PAGE, request.url));
   } else if (
     pathname !== LOGIN_PAGE &&
     (rtToken === undefined || token?.error === "RefreshAccessTokenError")
@@ -71,75 +72,3 @@ export const config = {
     "/svc-type-list/:path*",
   ],
 };
-
-// import { getToken } from "next-auth/jwt";
-// import { NextRequest, NextResponse } from "next/server";
-//
-// // This function can be marked `async` if using `await` inside
-// const LOGIN_PAGE = "/sign-in";
-// export async function middleware(request: NextRequest) {
-//   const { pathname } = request.nextUrl;
-//   const { matcher } = config;
-//
-//   // eslint-disable-next-line turbo/no-undeclared-env-vars
-//   // console.log("$$$", process.env.NEXT_PUBLIC_NEXTAUTH_SECRET)
-//   // eslint-disable-next-line turbo/no-undeclared-env-vars
-//   console.log("process.env.NEXTAUTH_URL", process.env.NEXTAUTH_URL);
-//
-//   //Page url 외의 요청은 Pass한다.
-//   // if (!matcher.includes(pathname)) {
-//   //   return NextResponse.next();
-//   // }
-//
-//   const token = await getToken({
-//     req: request,
-//     // eslint-disable-next-line turbo/no-undeclared-env-vars
-//     secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
-//   });
-//
-//   // console.log('token > ', token?.error);
-//
-//   const rtToken = token?.refreshToken;
-//
-//   if (
-//     pathname === LOGIN_PAGE &&
-//     rtToken !== undefined &&
-//     token?.error !== "RefreshAccessTokenError"
-//   ) {
-//     return NextResponse.rewrite(new URL("/", request.url));
-//   } else if (
-//     pathname !== LOGIN_PAGE &&
-//     (rtToken === undefined || token?.error === "RefreshAccessTokenError")
-//   ) {
-//     return NextResponse.redirect(new URL("/sign-in", request.url));
-//   } else {
-//     return NextResponse.next();
-//   }
-// }
-//
-// export const config = {
-//   matcher: [
-//     "/",
-//     "/dashboard",
-//     "/sign-in",
-//     "/order-list/:path*",
-//     "/order-reg/:path*",
-//     "/agnc-pi-list/:path*",
-//     "/agnc-pi-add/:path*",
-//     "/agnc-pi-modify/:path*",
-//     "/cust-list/:path*",
-//     "/cust-modify/:path*",
-//     "/inst-info-list/:path*",
-//     "/inst-info-add/:path*",
-//     "/inst-info-modify/:path*",
-//     "/contact-list/:path*",
-//     "/es-pr-list/:path*",
-//     "/machine-kit-list/:path*",
-//     "/master-code-list/:path*",
-//     "/project-list/:path*",
-//     "/svc-cate-add/:path*",
-//     "/svc-cate-list/:path*",
-//     "/svc-std-price-list/:path*",
-//     "/svc-type-list/:path*",
-//   ],
-// };
